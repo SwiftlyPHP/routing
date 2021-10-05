8b9f1349ecf30b79a66cfcd9c1b75af774ecda7b
clvarley <conorlukevarley@gmail.com>
2021-10-05 19:40:49 +0100

    Very basic tests for the cached collection

--- tests/Collection/CachedCollectionTest.php
+++ tests/Collection/CachedCollectionTest.php
@@ -10,4 +10,26 @@ use PHPUnit\Framework\TestCase;
  */
 Class CachedCollectionTest Extends TestCase
 {
+
+    const COMPILED_REGEX = [
+        'GET' => '~^(?|(?>/(*:home))|(?>/form/[s:name](*:form)))$~ixX',
+        'POST' => '~^(?|(?>/post/[i:id](*:post))|(?>/form/[s:name](*:form)))$~ixX'
+    ];
+
+    /** @var CachedCollection $collection */
+    private $collection;
+
+    protected function setUp() : void
+    {
+        $this->collection = new CachedCollection( self::COMPILED_REGEX );
+    }
+
+    public function testReturnsCachedRegexForMethod() : void
+    {
+        foreach ( self::COMPILED_REGEX as $http_method => $expected ) {
+            $regex = $this->collection->compile( $http_method );
+
+            self::assertSame( $expected, $regex );
+        }
+    }
 }
