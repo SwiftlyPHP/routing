5e7702f75c78f326ee2875c647f93fd31609959f
clvarley <conorlukevarley@gmail.com>
2021-10-06 22:53:41 +0100

    Removed regex step from the JSON parser

--- src/Parser/JsonParser.php
+++ src/Parser/JsonParser.php
@@ -7,22 +7,18 @@ use Swiftly\Routing\CollectionInterface;
 use Swiftly\Routing\ParserInterface;
 use Swiftly\Routing\Route;
 
-use function rtrim;
 use function is_readable;
-use function is_array;
 use function file_get_contents;
-use function array_filter;
-use function array_intersect;
-use function is_string;
-use function explode;
 use function json_decode;
 use function json_last_error;
-use function preg_match_all;
-use function preg_quote;
+use function is_array;
+use function rtrim;
+use function array_intersect;
+use function array_filter;
+use function is_string;
+use function explode;
 
 use const JSON_ERROR_NONE;
-use const PREG_SET_ORDER;
-use const PREG_OFFSET_CAPTURE;
 
 /**
  * Class responsible for loading routes from .json files
@@ -118,7 +114,7 @@ Class JsonParser Implements ParserInterface
         $route = new Route;
 
         // Parse regex
-        $route->regex = $this->compileRegex( $json['path'], $route );
+        $route->raw = rtrim( $json['path'], " \n\r\t\0\x0B\\/" );
 
         // Allowed HTTP verbs only
         if ( !empty( $json['methods'] ) && is_array( $json['methods'] ) ) {
@@ -140,64 +136,5 @@ Class JsonParser Implements ParserInterface
         }
 
         return $route;
-    }
-
-    /**
-     * Parse route and build the neccessary regex
-     *
-     * @param string $path Route path
-     * @param Route $route Current route
-     * @return string      Compiled regex
-     */
-    private function compileRegex( string $path, Route $route ) : string
-    {
-        $path = rtrim( $path, " \n\r\t\0\x0B\\/" );
-
-        // No route, assume root
-        if ( empty( $path ) ) {
-            return '/';
-        }
-
-        // Gather any route placeholders?
-        if ( !preg_match_all( self::ROUTE_REGEX, $path, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE ) ) {
-            $path;
-        }
-
-        $regex = '';
-
-        /**
-         * Reference:
-         *
-         * $match[0]      - The entire match, ie: '[i:name]'
-         * $match['name'] - The name of the placeholder
-         * $match['type'] - The type of the placeholder
-         */
-        foreach ( $matches as $match ) {
-            if ( empty( $match['name'] ) ) {
-                $regex .= preg_quote( $match[0][0] );
-                continue;
-            }
-
-            // Atomic groups were messing with names :(
-            $regex .= '(';
-
-            // Use appropriate regex
-            switch ( $match['type'][0] ) {
-                case 'i':
-                    $regex .= '\d+';
-                    break;
-
-                case 's':
-                default:
-                    $regex .= '[a-zA-Z0-9-_]+';
-                    break;
-            }
-
-            $regex .= ')';
-
-            $route->args[] = $match['name'][0];
-        }
-
-        return $regex;
     }
 }
