9b4a6fbba78440a278453cf90792d1a414241561
clvarley <conorlukevarley@gmail.com>
2021-10-25 13:11:40 +0100

    New provider interface to replace current json-parser

---
+++ src/ProviderInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Swiftly\Routing;
+
+use Swiftly\Routing\Collection\RouteCollection;
+
+/**
+ * Interface for classes that can provide/load route definitions
+ *
+ * @author clvarley
+ */
+Interface ProviderInterface
+{
+
+    /**
+     * Populate the collection with routes from this provider
+     *
+     * @psalm-flow ($collection) -> return
+     *
+     * @param RouteCollection $collection Route collection
+     * @return RouteCollection            Updated collection
+     */
+    public function populate( RouteCollection $collection ) : RouteCollection;
+
+}
