11017ad217fb7275f25d497cdee9de57416ac9b3
clvarley <conorlukevarley@gmail.com>
2021-10-26 21:19:45 +0100

    Potential interfaces for future dev

---
+++ src/CompilerInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Swiftly\Routing;
+
+use Swiftly\Routing\Collection\RouteCollection;
+use Swiftly\Routing\MatcherInterface;
+
+/**
+ * Interface for classes that can compile/create route matchers
+ *
+ * @author clvarley
+ */
+Interface CompilerInterface
+{
+
+    /**
+     * Compile the appropriate matcher for the given routes
+     *
+     * @param RouteCollection $routes Route collection
+     * @return MatcherInterface       Compiled matcher
+     */
+    public function compile( RouteCollection $routes ) : MatcherInterface;
+
+}
---
+++ src/MatcherInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Swiftly\Routing;
+
+use Swiftly\Routing\Route;
+
+/**
+ * Interface for classes that can match URLs to routes
+ *
+ * @author clvarley
+ */
+Interface MatcherInterface
+{
+
+    /**
+     * Attempt to find a route that satisfies the given URL
+     *
+     * @param string $url Requested URL
+     * @return Route|null Matching route
+     */
+    public function match( string $url ) : ?Route;
+
+}
