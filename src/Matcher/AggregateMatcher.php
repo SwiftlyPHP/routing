27a372e9830cc89c7569650fa197e75af2d69321
clvarley <conorlukevarley@gmail.com>
2021-10-26 21:48:45 +0100

    Created an aggregate matcher

---
+++ src/Matcher/AggregateMatcher.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Swiftly\Routing\Matcher;
+
+use Swiftly\Routing\MatcherInterface;
+use Swiftly\Routing\Route;
+
+/**
+ * Class that calls matchers in series until a route is found
+ */
+Class AggregateMatcher Implements MatcherInterface
+{
+
+    /** @var MatcherInterface[] $matchers Route matchers */
+    private $matchers;
+
+    /**
+     * Creates a new aggregator around the given matchers
+     *
+     * @param MatcherInterface[] $matchers Route matchers
+     */
+    public function __construct( array $matchers )
+    {
+        $this->matchers = $matchers;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match( string $url ) : ?Route
+    {
+        foreach ( $this->matchers as $matcher ) {
+            $route = $matcher->match( $url );
+
+            if ( $route !== null ) {
+                return $route;
+            }
+        }
+
+        return null;
+    }
+}
