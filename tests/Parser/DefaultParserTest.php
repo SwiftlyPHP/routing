be20d8c625e250b656ec6ab99ed6b12357b0ae81
clvarley <conorlukevarley@gmail.com>
2022-10-19 19:23:31 +0100

    Wrote tests for the standard parser

---
+++ tests/Parser/DefaultParserTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Swiftly\Routing\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Swiftly\Routing\Parser\DefaultParser;
+use Swiftly\Routing\ComponentInterface;
+use Swiftly\Routing\Exception\ParsingException;
+
+Class DefaultParserTest Extends TestCase
+{
+    /** @var DefaultParser $parser */
+    private $parser;
+
+    public function setUp(): void
+    {
+        $this->parser = new Parser();
+    }
+
+    /**
+     * Assert that an array contains only strings and {@see ComponentInterface} types
+     */
+    private static function assertContainsOnlyStringsAndComponents(array $subject): void
+    {
+        self::assertThat(
+            $subject,
+            self::logicalOr(
+                self::containsOnly('string'),
+                self::containsOnlyInstancesOf(ComponentInterface::class)
+            )
+        );
+    }
+
+    public function testCanParseStaticUrl(): void
+    {
+        $components = $this->parser->parse('/admin/users');
+
+        self::assertIsArray($components);
+        self::assertCount(1, $components);
+        self::assertContainsOnly('string', $components);
+        self::assertContains('/admin/users');
+    }
+
+    public function testCanParseDynamicUrl(): void
+    {
+        $components = $this->parser->parse('/post/[s:category]/[s:slug]');
+
+        self::assertIsArray($components);
+        self::assertCount(4, $components);
+        self::assertContainsOnlyStringsAndComponents($components);
+
+        self::assertArrayHasKey(0, $components);
+        self::assertSame('/posts/', $components[0]);
+
+        self::assertArrayHasKey(1, $components);
+        self::assertInstanceOf(ComponentInterface::class, $components[1]);
+
+        self::assertArrayHasKey(2, $components);
+        self::assertSame('/', $components[2]);
+
+        self::assertInstanceOf(3, $components);
+        self::assertInstanceOf(ComponentInterface::class, $components[3]);
+    }
+
+    public function testThrowsOnInvalidUrl(): void
+    {
+        self::expectException(ParsingException::class);
+
+        $this->parser->parse('@#://invalid');
+    }
+}
