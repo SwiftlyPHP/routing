c0c96ab7f1be3a5406e7b18564d0253282b1ebdc
clvarley <conorlukevarley@gmail.com>
2022-10-20 17:36:51 +0100

    Wrote tests for the integer component

---
+++ tests/Component/IntegerComponent.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Swiftly\Routing\Tests\Component;
+
+use PHPUnit\Framework\TestCase;
+use Swiftly\Routing\Component\IntegerComponent;
+use Swiftly\Routing\Exception\FormatException;
+
+Class IntegerComponentTest Extends TestCase
+{
+    /** @var IntegerComponent $component */
+    private $component;
+
+    public function setUp(): void
+    {
+        $this->component = new IntegerComponent('id');
+    }
+
+    public function testCanGetName(): void
+    {
+        $name = $this->component->name();
+
+        self::assertSame('id', $name);
+    }
+
+    public function testCanGetRegex(): void
+    {
+        $regex = $this->component->regex();
+
+        self::assertSame('(\d+)', $regex);
+    }
+
+    public function testCanCheckIfValueIsValid(): void
+    {
+        self::assertTrue($this->component->valid(42));
+        self::assertTrue($this->component->valid('42'));
+        self::assertFalse($this->component->valid(4.2));
+        self::assertFalse($this->component->valid('4.2'));
+        self::assertFalse($this->component->valid('fortytwo'));
+    }
+
+    public function testCanFormatValue(): void
+    {
+        $formatted = $this->component->format(42);
+
+        self::assertSame('42', $formatted);
+    }
+
+    public function testThrowsOnInvalidValue(): void
+    {
+        self::expectException(FormatException::class);
+
+        $this->component->format(4.2);
+    }
+}
