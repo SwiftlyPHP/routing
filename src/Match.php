001589d98320e65e86bacdb7b86aae9f925c096b
clvarley <conorlukevarley@gmail.com>
2022-10-22 19:49:01 +0100

    Created the match class to satisfy all tests

---
+++ src/Match.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Swiftly\Routing;
+
+use Swiftly\Routing\Route;
+
+/**
+ * Plain old data structure used to store information about a matched route
+ *
+ * @internal
+ *
+ * @psalm-immutable
+ */
+Class Match
+{
+    /**
+     * @psalm-var non-empty-string $name
+     * @var string $name
+     */
+    public $name;
+
+    /**
+     * @var Route $route
+     */
+    public $route;
+
+    /**
+     * @psalm-var array<string,string> $args
+     * @var string[] $args
+     */
+    public $args;
+
+    /**
+     * Associate a route with the given name and arguments
+     *
+     * @psalm-param non-empty-string $name
+     * @psalm-param array<string,string> $args
+     *
+     * @param string $name   Route name
+     * @param Route $route   Matched route
+     * @param string[] $args Matched arguments
+     */
+    public function __construct(string $name, Route $route, array $args = [])
+    {
+        $this->name = $name;
+        $this->route = $route;
+        $this->args = $args;
+    }
+}
