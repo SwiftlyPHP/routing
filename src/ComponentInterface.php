0c4ba2eed3914d7ef640ccfa0ccc56d113b1065d
clvarley <conorlukevarley@gmail.com>
2022-10-22 20:00:28 +0100

    Created the interface all URL components must adhere to

--- src/ComponentInterface.php
+++ src/ComponentInterface.php
@@ -2,10 +2,46 @@
 
 namespace Swiftly\Routing;
 
+use Swiftly\Routing\Exception\FormatException;
+
 /**
- * 
+ * Interface representing a flexible/variable part of a URL
  */
 Interface ComponentInterface
 {
+    /**
+     * Return the name of this URL component
+     *
+     * @psalm-return non-empty-string
+     *
+     * @return string
+     */
+    public function name(): string;
 
+    /**
+     * Return the regex used to match values for this component
+     *
+     * @psalm-return non-empty-string
+     *
+     * @return string
+     */
+    public function regex(): string;
+
+    /**
+     * Determine if the given value is accepted by this component
+     *
+     * @param mixed $value Value to check
+     */
+    public function accepts($value): bool;
+
+    /**
+     * Attempt to escape the given value for use in a URL
+     *
+     * @psalm-return non-empty-string
+     *
+     * @throws FormatException If value cannot be escaped
+     * @param mixed $value     Value to format
+     * @return string
+     */
+    public function escape($value): string;
 }
