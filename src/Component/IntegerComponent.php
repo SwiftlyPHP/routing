dbf1a9ab1fa2be4278c77422a0296b0887b8ef03
clvarley <conorlukevarley@gmail.com>
2023-03-21 14:27:39 +0000

    Created IntegerComponent from test spec

---
+++ src/Component/IntegerComponent.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Swiftly\Routing\Component;
+
+use Swiftly\Routing\ComponentInterface;
+use Swiftly\Routing\Exception\FormatException;
+
+use function filter_var;
+
+use const FILTER_VALIDATE_INT;
+
+/**
+ * 
+ *
+ * @psalm-immutable
+ */
+class IntegerComponent implements ComponentInterface
+{
+    /**
+     * @psalm-param non-empty-string $name
+     * @param string $name
+     */
+    private $name;
+
+    /**
+     * Create a new numeric only URL component
+     * 
+     * @psalm-param non-empty-string $name
+     *
+     * @param string $name      Component name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function regex(): string
+    {
+        return "(\d+)";
+    }
+
+    public function accepts($value): bool
+    {
+        return filter_var($value, FILTER_VALIDATE_INT) !== false;
+    }
+
+    public function escape($value): string
+    {
+        if (!$this->accepts($value)) {
+            throw new FormatException($this->name(), $value);
+        }
+
+        return (string)$value;
+    }
+}
