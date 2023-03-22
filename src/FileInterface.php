63516020f49cef65fd0832786c9f0eb3de145b04
clvarley <conorlukevarley@gmail.com>
2023-03-22 16:20:50 +0000

    Fixed missing @throws docblock

--- src/FileInterface.php
+++ src/FileInterface.php
@@ -3,6 +3,7 @@
 namespace Swiftly\Routing;
 
 use Swiftly\Routing\Exception\FileReadException;
+use Swiftly\Routing\Exception\FileParseException;
 
 /**
  * Interface to signify classes capable of loading files from the filesystem
@@ -12,7 +13,8 @@ Interface FileInterface
     /**
      * Load this file, perform any required processing and return its content
      *
-     * @throws FileReadException If file cannot be read
+     * @throws FileReadException  If file cannot be read
+     * @throws FileParseException If the file cannot be parsed
      * @return mixed[]
      */
     public function load(): array;
